251020a0971f1732bd971267ee7c3c79437054c1
Prathmesh Patil <prathmesh.patil29@outlook.com>
2024-08-17 13:36:35 +0530

    #Design changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     Url_api : 'https://portfolio-api-owp2.onrender.com/api'
-    //Url : 'https://localhost:44357/api'
+    //Url_api : 'https://localhost:44357/api'
 };
